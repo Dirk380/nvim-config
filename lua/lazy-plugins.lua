38de52f7054f59bafdab4a0c645c41b8712b8687
dirk380 <dirkvmunster@gmail.com>
2024-04-04 14:35:58 +0200

    Added spectre for find and replace

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -7,7 +7,9 @@
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
   -- Git related plugins
-   'vim-test/vim-test',
+  'nvim-lua/plenary.nvim',
+  'nvim-pack/nvim-spectre',
+  'vim-test/vim-test',
   {
     'nvim-neotest/neotest',
     dependencies = {
