c13e96024c7d42ed03749d36ba2f7e009059add2
dirk380 <dirkvmunster@gmail.com>
2024-06-04 11:54:31 +0200

    Removed vim-fugitive

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,7 +12,7 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-fugitive',
+  -- 'tpope/vm-fugitive',
   'akinsho/toggleterm.nvim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
